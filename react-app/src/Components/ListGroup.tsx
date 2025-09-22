9acb9945e8c24eb3a21c8ef1f4b8b7a56193b5c1
fabianoGDB <doctorfabi.f@gmail.com>
2025-09-22 16:37:49 -0300

    A010: using funtion to render conditional elements the pratice is used when you have to pass parameters

--- react-app/src/Components/ListGroup.tsx
+++ react-app/src/Components/ListGroup.tsx
@@ -2,12 +2,14 @@ import { Fragment } from "react";
 
 function ListGroup() {
   const items = ["New York", "San Francisco", "Tokyo", "London", "Los Angeles"];
-  const message = items.length === 0 ? <p>No items found</p> : null;
+  const getMessage = () => {
+    items.length === 0 ? <p>No items found</p> : null;
+  };
 
   return (
     <>
       <h1>List</h1>
-      {message}
+      {getMessage}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item} className="list-group-item">
